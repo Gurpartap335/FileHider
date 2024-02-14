8cb5ae2032d26aae2c41023a38745178b8c4e0f9
Gurpartap Singh <sgurpartap880@gmail.com>
2024-02-14 20:40:02 +0530

    add the feature to hide images also

--- src/main/java/dao/DataDAO.java
+++ src/main/java/dao/DataDAO.java
@@ -3,6 +3,7 @@ package dao;
 import db.MyConnection;
 import model.Data;
 
+import javax.activation.MimetypesFileTypeMap;
 import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
@@ -39,12 +40,23 @@ public class DataDAO {
             ps.setString(3, file.getEmail());
 
             File f = new File(file.getPath());
-            FileReader fileReader = new FileReader(f);
-            ps.setCharacterStream(4, fileReader, f.length());
-            ps.executeUpdate();
-            fileReader.close();
+
+            if (isImage(f)) {
+                try (InputStream in = new FileInputStream(f)) {
+                    ps.setBinaryStream(4, in, f.length());
+                    ps.executeUpdate();
+                }
+            } else {
+                FileReader fileReader = new FileReader(f);
+                ps.setCharacterStream(4, fileReader, f.length());
+                ps.executeUpdate();
+                fileReader.close();
+            }
+
             if (f.delete()) {
-                System.out.println(file.getFileName() + "is hidden now");
+                System.out.println(file.getFileName() + " is hidden now");
+            } else {
+                System.out.println(file.getFileName() + " failed to hide");
             }
         } catch (SQLException | IOException e) {
             e.printStackTrace();
@@ -55,28 +67,49 @@ public class DataDAO {
     public static void unHide(int id) {
 
         try (Connection connection = MyConnection.getConnection()) {
-            PreparedStatement ps = connection.prepareStatement("Select path, bin_data from data where id = ?");
+            PreparedStatement ps = connection.prepareStatement("Select path, bin_data, name from data where id = ?");
             ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
             rs.next();
             String path = rs.getString("path");
-            Clob c = rs.getClob("bin_data");
+            String fileName = rs.getString("name");
+            File file = new File(path);
+            if (isImage(file)) {
+                try (FileOutputStream fos = new FileOutputStream(file)) {
+                    Blob blob = rs.getBlob("bin_data");
+                    byte[] b = blob.getBytes(1, (int) blob.length());
+                    fos.write(b);
+                }
 
-            Reader r = c.getCharacterStream();
-            FileWriter fw = new FileWriter(path);
-            int i;
-            while ((i = r.read()) != -1) {
-                fw.write((char) i);
+            } else {
+                Clob clob = rs.getClob("bin_data");
+
+                Reader r = clob.getCharacterStream();
+                FileWriter fw = new FileWriter(path);
+                int i;
+                while ((i = r.read()) != -1) {
+                    fw.write((char) i);
+                }
+                fw.close();
             }
-            fw.close();
 
             ps = connection.prepareStatement("delete from data where id = ?");
             ps.setInt(1, id);
             ps.executeUpdate();
             ps.close();
-            System.out.println("Unhidden");
+            System.out.println(fileName + " is now Unhidden");
         } catch (SQLException | IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static boolean isImage(File file) {
+        String mimeType = new MimetypesFileTypeMap().getContentType(file);
+        String type = mimeType.split("/")[0];
+        if (type.equals("image")) {
+            return true;
+        } else {
+            return false;
         }
     }
 
